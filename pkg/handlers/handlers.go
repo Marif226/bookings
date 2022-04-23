7050adbb3b56cdf8adb186fada14cfb182aa97c0
Marif <marif226@mail.ru>
2022-04-23 16:54:12 +0600

    Added go template layout for pages

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -48,3 +48,36 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+// Reservation renders the make a reservation page and displays form
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{
+
+	})
+}
+// Generals renders the generals room page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{
+		
+	})
+}
+// Majors renders the majors room page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.html", &models.TemplateData{
+		
+	})
+}
+
+// Availability renders the search availability room page
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.html", &models.TemplateData{
+		
+	})
+}
+
+// Contact renders the contact page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.html", &models.TemplateData{
+		
+	})
+}
